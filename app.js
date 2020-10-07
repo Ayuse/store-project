552438e185d1fd23f1c646631ca17dec908774d8
Ayodele Oluwasina <ayodeleoluwasina@gmail.com>
2020-10-08 00:19:36 +0100

    commit on main

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ const ui = new UI();
 
 fakestore.getItem().then((data) => {
   console.log(data);
-  data.map((data) => {
-    ui.showItem(data.item);
-  });
-  console.log(data.storeData[1]);
+  show(data);
 });
+
+function show(data) {
+  let item = ` <div class="item">
+  <img class="img" src="" alt="" />
+  <p>Item 1</p>
+  <a href="">www.item1.com</a>
+</div>`;
+
+  for (let r of data.storeData) {
+    item += `
+  <div class="item">
+  <img class="img" src="${r.image}" alt="" />
+  <h1>${r.title}</h1>
+  <p>${r.description}</p>
+  <p>${r.price}</p>
+</div>
+  `;
+  }
+  document.getElementById('itemDisplay').innerHTML = item;
+}
