0b009da12dd3ac2c692acfff7b3ca296369b8b9e
Ayodele Oluwasina <ayodeleoluwasina@gmail.com>
2020-10-14 08:55:30 +0100

    new

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-const fakestore = new Fakestore();
-const ui = new UI();
+const search = document.getElementById('search-box');
+let storeData = [];
 
-fakestore.getItem().then((data) => {
-  console.log(data);
-  const response = data;
-  if (response) {
-    hideloader();
+const getStore = async () => {
+  try {
+    const storeItem = await fetch(`https://fakestoreapi.com/products`);
+    storeData = await storeItem.json();
+    console.log(storeData);
+  } catch (err) {
+    console.error(err);
   }
-  show(data);
-});
+};
 function hideloader() {
   document.getElementById('loading').style.display = 'none';
 }
 
-function show(data) {
-  let item = ``;
+// search.addEventListener('keyup', (e) => {
+//   const searchString = e.target.value.toLowerCase();
+//   console.log(searchString);
+//   const filteredStore = storeData.filter((storeitem) => {
+//     return storeitem.title.includes(searchString);
+//   });
+//   console.log(filteredStore);
+// });
+// function show(data) {
+//   let item = ``;
 
-  for (let r of data.storeData) {
-    item += `
-  <div class="item">
-  <img class="img" src="${r.image}" alt="" />
-  <h1>${r.title}</h1>
-  <p>${r.description}</p>
-  <p>${r.price}</p>
-</div>
-  `;
-  }
-  document.getElementById('itemDisplay').innerHTML = item;
-}
+//   for (let r of data.storeData) {
+//     item += `
+//   <div class="item">
+//   <img class="img" src="${r.image}" alt="" />
+//   <h1>${r.title}</h1>
+//   <p>${r.description}</p>
+//   <p>${r.price}</p>
+// </div>
+//   `;
+//   }
+//   document.getElementById('itemDisplay').innerHTML = item;
+// }
